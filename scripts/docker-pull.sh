2350302125568365e32bed303fa8bcaa11a3fed3
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2015-04-28 11:45:33 -0400

    Make Docker pull script more robust

--- scripts/docker-pull.sh
+++ scripts/docker-pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-# Clone repo for the first time
+# Clone the repo for the first time.
 if [[ ! -d /tmp/$IMAGE || ! -d /tmp/$IMAGE/.git ]]; then
 	mkdir -p /tmp/$IMAGE
 	git clone -b $BRANCH $REPO /tmp/$IMAGE
@@ -8,8 +9,22 @@ fi
 
 cd /tmp/$IMAGE
 
+# Fix permissions.
 sudo chown -R $USER:$USER ./
-git reset --hard
-git clean -f -d
-git checkout $BRANCH || git checkout -b $BRANCH origin/$BRANCH
-git pull
+
+# Clean up the repository.
+git reset --hard && git clean -f -d
+
+# Switch to master and update.
+git checkout master && git pull
+
+# Remove all branches except for master.
+# (This prevents error when someone rebases his branch.)
+for branch in $(git branch | grep -v master || :); do
+	git branch -D $branch
+done
+
+# If we're deploying a custom branch, we need to pull it first.
+if [ "$BRANCH" != "master" ]; then
+	git checkout -b $BRANCH origin/$BRANCH
+fi
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ ! -f $CONFIG ]; then
 	echo "\"$CONFIG\" file missing"
@@ -8,7 +9,7 @@ fi
 sudo docker run -d \
 	-p $HOST_PORT:$CONTAINER_PORT \
 	-v $CONFIG:/etc/imgry.conf \
-  -v /data:/data \
-  --memory-swap=-1 \
+	-v /data:/data \
+	--memory-swap=-1 \
 	--restart=always \
 	--name $NAME $IMAGE
