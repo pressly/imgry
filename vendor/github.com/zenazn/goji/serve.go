4715388abe0c86dc8ecfdc0aa632f14581d22c12
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-07-11 16:21:45 -0400

    Upgrade goji

--- vendor/github.com/zenazn/goji/serve.go
+++ vendor/github.com/zenazn/goji/serve.go
@@ -3,8 +3,10 @@
 package goji
 
 import (
+	"crypto/tls"
 	"flag"
 	"log"
+	"net"
 	"net/http"
 	"time"
 
@@ -26,12 +28,25 @@ func Serve() {
 		flag.Parse()
 	}
 
+	ServeListener(bind.Default())
+}
+
+// Like Serve, but enables TLS using the given config.
+func ServeTLS(config *tls.Config) {
+	if !flag.Parsed() {
+		flag.Parse()
+	}
+
+	ServeListener(tls.NewListener(bind.Default(), config))
+}
+
+// Like Serve, but runs Goji on top of an arbitrary net.Listener.
+func ServeListener(listener net.Listener) {
 	DefaultMux.Compile()
 	// Install our handler at the root of the standard net/http default mux.
 	// This allows packages like expvar to continue working as expected.
 	http.Handle("/", DefaultMux)
 
-	listener := bind.Default()
 	log.Println("Starting Goji on", listener.Addr())
 
 	graceful.HandleSignals()
